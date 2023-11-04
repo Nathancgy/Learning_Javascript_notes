393f65fe81c9b69b2e77a19fa7f1afc7ce672453
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-11-04 16:32:40 +0800

    added countries_dropdown

---
+++ Countries_dropdown/countries.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} countries
+ */
+export const getDropdown = (countries) => {
+    console.log(countries);
+    return `<option value="">Please select</option>
+    ${countries.map(country=>`<option value="${country.toLowerCase()}">${country}</option>`)}`;
+}

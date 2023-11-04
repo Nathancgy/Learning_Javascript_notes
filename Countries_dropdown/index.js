717f018a1b0393a7ed675c66eb60614bba08120d
Nathancgy <guangyu.chen40730-biph@basischina.com>
2023-11-04 16:42:48 +0800

    refined index.htlm

--- Countries_dropdown/index.js
+++ Countries_dropdown/index.js
@@ -1,8 +1,8 @@
-import {getDropdown} from "./countries.js";
+import {getDropdown} from "/Countreis_dropdown/countries.js";
 
 const countries = ["Netherlands", "Japan", "Mongolia"];
 
 const html = getDropdown(countries);
-
+console.log(html);
 const select = document.querySelector("#countries-dropdown");
 select.innerHTML = html;
